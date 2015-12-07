c06c8d0a8d826bfe6df524dddd7b98e763f156d9
jmstenkv <jmstenkv@ncsu.edu>
2015-12-07 14:29:35 -0500

    Added package library ; first commit

---
+++ package.js
@@ -0,0 +1,45 @@
+// https://github.com/planet-training/meteor-angular-material
+
+Package.describe({
+  name: 'mystec:angular-nvd3',
+  version: '1.0.5',
+  // Brief, one-line summary of the package.
+  summary: 'Angular Directives for nvD3 charts',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/krispo/angular-nvd3',
+
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+
+  api.use(["angular:angular@1.4.6"], "client");
+
+  api.addFiles([
+    /* nvd3 base packages */
+    //'client/bower_components/angular/angular.min.js',
+    'client/bower_components/d3/d3.min.js',
+    'client/bower_components/nvd3/build/nv.d3.js',
+    'client/bower_components/angular-nvd3/dist/angular-nvd3.min.js',
+    'client/bower_components/nvd3/build/nv.d3.css',
+  ], "client");
+});
+
+
+/**
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('angular-nvd3.js');
+});
+
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('mystec:angular-nvd3');
+  api.addFiles('angular-nvd3-tests.js');
+});
+*/
